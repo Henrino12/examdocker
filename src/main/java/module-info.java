ea02fdb490434b687aa0719c63afe0065edb9ea6
KONE Henri Noel <sabsonkone@gmail.com>
2024-04-09 20:36:47 +0000

    docker

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module com.example.examendevops {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+
+
     opens com.example.examendevops to javafx.fxml;
     exports com.example.examendevops;
 }
